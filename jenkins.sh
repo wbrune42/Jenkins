860f5e363fc7fb628827ba9fb259e670c8d29628
wbrune <ihre@emailadresse.de>
2017-03-10 18:50:31 +0100

    move docker.sock from /var/run/docker to /run/docker

--- jenkins.sh
+++ jenkins.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -d -p 8080:8080 -p 50000:50000 -v /opt/docker/jenkins:/var/jenkins_home -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock wbrune/jenkins
+#docker run -d -p 8080:8080 -p 50000:50000 -v /opt/docker/jenkins:/var/jenkins_home -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock wbrune/jenkins
+docker run -d -p 8080:8080 -p 50000:50000 -v /opt/docker/jenkins:/var/jenkins_home -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/run/docker.sock wbrune/jenkins
