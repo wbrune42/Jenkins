fbddc00a9d2d9b953954a5d8558b67dd9200469c
Willi Brune <wbrune@nicos-ag.com>
2017-09-08 15:47:04 +0200

    run registry.int.nsc.ag:5000/jenkins:1.0.84

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,6 @@
 #docker run -d -p 8080:8080 -p 50000:50000 -v /opt/docker/jenkins:/var/jenkins_home -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/run/docker.sock wbrune/jenkins
 #docker run -d -p 8080:8080 -p 50000:50000 -v /opt/docker/jenkins-test:/var/jenkins_home -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/run/docker.sock registry.int.nsc.ag:5000/jenkins
 #docker run -d -p 8080:8080 -p 50000:50000 -v /opt/docker/jenkins:/var/jenkins_home -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/run/docker.sock registry.int.nsc.ag:5000/jenkins:1.0.71
-docker run -d -p 8080:8080 -v /opt/docker/jenkins:/var/jenkins_home -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/run/docker.sock registry.int.nsc.ag:5000/jenkins:1.0.80
+docker run -d -p 8080:8080 -v /opt/docker/jenkins:/var/jenkins_home -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/run/docker.sock --name jenkins registry.int.nsc.ag:5000/jenkins:1.0.84
 #docker run -d -p 8080:8080 -p 50000:50000 -v /opt/docker/jenkins:/var/jenkins_home -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/run/docker.sock wbrune/jenkins
 #docker run -d -p 8080:8080 -p 50000:50000 -v /opt/docker/jenkins:/var/jenkins_home -v /usr/bin/docker:/usr/bin/docker -v /var/run/docker.sock:/run/docker.sock wbrune/jenkins
